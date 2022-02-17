c456580fe3d81f2109af92e63c0104b848cddd24
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-02-16 22:19:09 -0600

    router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import Navbar from './layout/Navbar';
+import React , {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import './App.css';
 
 const App = () => {
     return (
-        <div className="App">
-            <Navbar/>
-            My app
-        </div>
+        <Router>
+            <Fragment>
+                <Navbar/>
+                <div className='container'>
+                    <Routes>
+                        <Route exact path='/' element = {<Home/>}/>
+                        <Route exact path='/about' element = {<About/>}/>
+                    </Routes>
+                </div>
+            </Fragment>
+        </Router>
     );
 }
 
