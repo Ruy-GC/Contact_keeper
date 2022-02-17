1056a9f4058ac2b0810b1e509e46a612077f58aa
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-02-16 21:35:59 -0600

    client initial setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './layout/Navbar';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Navbar/>
+            My app
+        </div>
+    );
+}
+
+export default App;
