353a21d7f0e9004bcc2a2c142a9c0a9487487ab2
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-02-17 15:43:17 -0600

    contacts initial context

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,27 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+
+//context
+import ContactState from './context/contact/ContactState';
+
 import './App.css';
 
 const App = () => {
     return (
-        <Router>
-            <Fragment>
-                <Navbar/>
-                <div className='container'>
-                    <Routes>
-                        <Route exact path='/' element = {<Home/>}/>
-                        <Route exact path='/about' element = {<About/>}/>
-                    </Routes>
-                </div>
-            </Fragment>
-        </Router>
+        <ContactState>
+            <Router>
+                <Fragment>
+                    <Navbar/>
+                    <div className='container'>
+                        <Routes>
+                            <Route exact path='/' element = {<Home/>}/>
+                            <Route exact path='/about' element = {<About/>}/>
+                        </Routes>
+                    </div>
+                </Fragment>
+            </Router>
+        </ContactState>
     );
 }
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,69 @@
+import React, {useReducer} from "react";
+import uuid from 'uuid';
+import ContactContext from "./contactContext";
+import contactReducer from "./ContactReducer";
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'Pepe',
+                email: 'pepe@gmail.com',
+                phone: '111-111-111',
+                type: 'professional'
+            },
+            {
+                id: 2,
+                name: 'Juan',
+                email: 'juan@gmail.com',
+                phone: '222-222-222',
+                type: 'personal'
+            },
+            {
+                id: 3,
+                name: 'Carlos',
+                email: 'carlos@gmail.com',
+                phone: '333-333-333',
+                type: 'professional'
+            },
+        ]
+    };
+
+    const [state,dispatch] = useReducer(contactReducer, initialState);
+
+    //ad contact
+    
+    //delete contact
+
+    //set current contact
+
+    //clear current contact
+
+    //update contact
+
+    //filter contacts
+
+    //clear filter
+
+    return (
+        <ContactContext.Provider
+            value = {{
+                contacts: state.contacts
+            }}
+        >
+            {props.children}
+        </ContactContext.Provider>
+    )
+};
+
+export default ContactState;
